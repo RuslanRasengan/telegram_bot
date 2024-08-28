4a80d2f3f9e996791e58c32fe181329c716cac21
Ruslan Rasengan <61047860+RuslanRasengan@users.noreply.github.com>
2024-08-28 20:47:26 +0300

    added git ignore and updated function UpdateString

--- TGBot/Telegram_Messanging_Bot/Program.cs
+++ TGBot/Telegram_Messanging_Bot/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
@@ -6,13 +7,36 @@ using Telegram.Bot.Types.Enums;
 
 namespace Telegram_Messanging_Bot
 {
+    struct BotUpdate
+    {
+        public string text;
+        public long id;
+        public string? username;
+
+    }
     class Program
     {
         static TelegramBotClient Bot = new TelegramBotClient
             ("7002027437:AAE846ngvBKppW1QcHl61wn58OFS_OPRq1A");
 
+        static string fileName = "updates.json";
+        static List<BotUpdate> botUpdates = new List<BotUpdate> ();
+
         static void Main(string[] args)
         {
+            //read all saved updates 
+
+            try
+            {
+                var botUpdateString = System.IO.File.ReadAllText(fileName);
+
+                botUpdates = JsonConvert.DeserializeObject<List<BotUpdate>>(botUpdateString) ??
+                    botUpdates;
+            }
+            catch(Exception ex) 
+            { 
+                Console.WriteLine($"Error reading or deserializing{ex}");  
+            }
             var receiverOptions = new ReceiverOptions
             {
                 AllowedUpdates = new UpdateType[]
@@ -36,17 +60,28 @@ namespace Telegram_Messanging_Bot
         private static async Task UpdateHandler(ITelegramBotClient bot, Update update, 
             CancellationToken token)
         {
-            if (update.Type == UpdateType.Message)
+            if (update.Type == UpdateType.Message && update.Message.Type == MessageType.Text)
             {
-                if (update.Message.Type == MessageType.Text)
+                var text = update.Message.Text.ToLower();
+                var id = update.Message.Chat.Id;
+                string? username = update.Message.Chat.Username;
+
+                switch (text)
                 {
-                    var text = update.Message.Text;
-                    var id = update.Message.Chat.Id;
-                    string? username = update.Message.Chat.Username;
+                    case "/start":
+                        await bot.SendTextMessageAsync(id, "Привіт! Я твій асистен. Чим я можу допомогти?");
+                        break;
 
-                    Console.WriteLine($"{username}| {id} | {text}");
+                    case "/help":
+                        await bot.SendTextMessageAsync(id, "Ось команди, які я розумію: /start, /help, /set_reminder");
+                        break;
 
+                    default:
+                        await bot.SendTextMessageAsync(id, "Вибачте, я не розумію цю командую");
+                        break;
                 }
+
+                Console.WriteLine($"{username}| {id} | {text}");
             }
         }
     }
--- TGBot/Telegram_Messanging_Bot/obj/Debug/net8.0/Telegram_Messanging_Bot.AssemblyInfo.cs
+++ TGBot/Telegram_Messanging_Bot/obj/Debug/net8.0/Telegram_Messanging_Bot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Telegram_Messanging_Bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+498253560e7ec58f2987f975dc06cef31f830b16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7aa9b6fe5e63bc8937810f69131404d58b57770")]
 [assembly: System.Reflection.AssemblyProductAttribute("Telegram_Messanging_Bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Telegram_Messanging_Bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
