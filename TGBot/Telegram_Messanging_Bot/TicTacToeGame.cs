5bf04d422481eaea7a4589b2492b0884c321a3bd
Ruslan Rasengan <61047860+RuslanRasengan@users.noreply.github.com>
2024-08-31 15:32:09 +0300

    bug fix, gitignore test

--- TGBot/Telegram_Messanging_Bot/TicTacToeGame.cs
+++ TGBot/Telegram_Messanging_Bot/TicTacToeGame.cs
@@ -8,19 +8,18 @@ namespace Telegram_Messanging_Bot
 {
     public class TicTacToeGame
     {
-        private readonly char[,] _board = new char[3, 3];
+        public char[,] _board { get; private set; }
 
-        private char _currentPlayer;
-
+        public char _currentPlayer { get; private set; }
+    
         public TicTacToeGame()
         {
-            _currentPlayer = 'X'; //Гра починається з гравця "Х"
-
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
                     _board[i, j] = ' ';
+                    _currentPlayer = 'X';
                 }
             }
         }
@@ -38,12 +37,63 @@ namespace Telegram_Messanging_Bot
                 return $"{_currentPlayer} виграв!";
             }
 
-            if (_currentPlayer = _currentPlayer == 'X' ? '0' : 'X';
-            {
-                return null; //Немає переможця, гра продовжєуться
-            }
+            _currentPlayer = _currentPlayer == 'X' ? '0' : 'X';
+
+            return null; //Немає переможця, гра продовжєуться
         }
 
-        
+        private bool CheckWinner(char player)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (_board[i, 0] == player && _board[i, 1] == player && _board[i, 2] == player)
+                {
+                    return true;
+                }
+                if (_board[0, i] == player && _board[1, i] == player && _board[2, i] == player)
+                {
+                    return true;
+                }
+            }
+            if (_board[0, 0] == player && _board[1, 1] == player && _board[2, 2] == player)
+            {
+                return true;
+            }
+            if (_board[0, 2] == player && _board[1, 1] == player && _board[2, 0] == player)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsBoardFull()
+        {
+            foreach(var cell in _board)
+            {
+                if (cell == ' ')
+                    return false;
+            }
+            return true;
+        }
+
+        public string GetBoard()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    sb.Append(_board[i, j]);
+                    if (j < 2)
+                    {
+                        sb.Append("|");
+                    }
+                }
+                sb.AppendLine();
+                if (i < 2) sb.AppendLine("-+-+-");
+            }
+            return sb.ToString();
+        }
     }
 }
--- TGBot/Telegram_Messanging_Bot/obj/Debug/net8.0/Telegram_Messanging_Bot.AssemblyInfo.cs
+++ TGBot/Telegram_Messanging_Bot/obj/Debug/net8.0/Telegram_Messanging_Bot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Telegram_Messanging_Bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cfaad51b96accdcefe695f11cb434025158c11a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1483a0c96b60f7cd3b8aabe4385adfc435757a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Telegram_Messanging_Bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Telegram_Messanging_Bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
